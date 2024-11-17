ab13ac26c6a1866755aa69f7a291162523fcb8fe
Dusty Panda <contact@dustypanda.com>
2024-11-17 03:46:46 +0100

    feat: minor - load events json through separate file

---
+++ content/events-data.js
@@ -0,0 +1,26 @@
+const eventsData = [
+  {
+    "imageUrl": "./content/psychedelic-stories.webp",
+    "linkUrl": "https://www.meetup.com/conscious-madrid/events/304216098/",
+    "title": "Psychedelic Stories: Experiences From The Other Side",
+    "dateStartString": "2024-11-27T19:10",
+    "location": "Palacio",
+    "description": [
+      "Curious about Psychedelics but unsure where to begin? Or perhaps you're an experienced psychonaut seeking to connect with others on a similar path?",
+      "This evening is a chance to meet others in <b>The Conscious Collective</b> community of open-minded adventurers and explorers, living in Madrid.",
+      "Join us for a series of <b>personal psychedelic stories</b>, shared by facilitators, explorers, and members of our community."
+    ]
+  },
+  {
+    "imageUrl": "./content/psychedelic-stories.webp",
+    "linkUrl": "https://www.meetup.com/conscious-madrid/events/304216157/",
+    "title": "Psychedelic Stories: Transformations & Healing",
+    "dateStartString": "2024-12-11T19:10",
+    "location": "Palacio",
+    "description": [
+      "Curious about Psychedelics but unsure where to begin? Or perhaps you're an experienced psychonaut seeking to connect with others on a similar path?",
+      "This evening is a chance to meet others in <b>The Conscious Collective</b> community of open-minded adventurers and explorers, living in Madrid.",
+      "Join us for a series of <b>personal psychedelic stories</b>, shared by facilitators, explorers, and members of our community."
+    ]
+  }
+];
